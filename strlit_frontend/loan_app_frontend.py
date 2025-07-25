35d9487d3ed3ce23a7947ec3628fc121f530afed
Harsh Bhosale <harshpbhosale215@gmail.com>
2025-07-25 16:18:17 +0530

    note

--- strlit_frontend/loan_app_frontend.py
+++ strlit_frontend/loan_app_frontend.py
@@ -2,7 +2,7 @@ import streamlit as st
 import requests
 from typing import Union, Dict, Any
 import requests
-url = "http://127.0.0.1:8000/api/predict/"
+url = "https://loanapprovalprediction-system.onrender.com"
 
 st.title("📊 Loan Approval Predictor")
 
